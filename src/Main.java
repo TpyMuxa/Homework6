b018d99e264e49c1ad8b2803e8e4095ba0443171
TpyMuxa <tpymuxa1133@gmail.com>
2023-09-05 22:25:12 +0500

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задание 1");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Переменная i = " + i);
+        }
+
     }
 }
