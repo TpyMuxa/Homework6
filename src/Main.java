c8ce42ace14147f8497f2a792657b58ac0d469d7
TpyMuxa <tpymuxa1133@gmail.com>
2023-09-05 22:38:48 +0500

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -31,5 +31,13 @@ public class Main {
                 System.out.println("Переменная i = " + i);
             }
         }
+
+        System.out.println("\nЗадание 5");
+
+        {
+            for (int i = 1904; i <= 2096; i = i + 4) {
+                System.out.println(i + " год является високосным");
+            }
+        }
     }
 }
